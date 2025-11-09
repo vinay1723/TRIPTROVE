e523230fc7aaa1b4f96540811b6fc4767047d748
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-11-09 11:38:09 +0530

    Update authController.js

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -19,6 +19,7 @@ const createSendToken = (user, statusCode, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    secure:true,
     sameSite: "Lax",
   };
   if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
