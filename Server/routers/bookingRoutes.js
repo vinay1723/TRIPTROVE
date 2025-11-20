75eee82f4308c654039313ca01262f0727e48f96
vinay1723 <vinaykumar.17july@gmail.com>
2025-11-20 19:08:01 +0530

    third commit

--- Server/routers/bookingRoutes.js
+++ Server/routers/bookingRoutes.js
@@ -1,23 +1,20 @@
-const express = require('express');
-const bookingController = require('./../controllers/bookingController');
-const authController = require('./../controllers/authController');
+const express = require("express");
+const bookingController = require("./../controllers/bookingController");
+const authController = require("./../controllers/authController");
 
 const router = express.Router();
 
-router.get(
-  '/checkout-session/:tourId',
-  bookingController.getCheckoutSession
-);
+router.use(authController.protect);
 
-// router.use(authController.protect);
+router.get("/checkout-session/:tourId", bookingController.getCheckoutSession);
 
 router
-  .route('/')
+  .route("/")
   .get(bookingController.getAllBookings)
   .post(bookingController.createBooking);
 
 router
-  .route('/:id')
+  .route("/:id")
   .get(bookingController.getBooking)
   .patch(bookingController.updateBooking)
   .delete(bookingController.deleteBooking);
