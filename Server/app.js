5adf48536514f27b7abda0a7c8993e0acaff0018
vinay1723 <vinaykumar.17july@gmail.com>
2025-12-17 17:48:34 +0530

    initial commit

--- Server/app.js
+++ Server/app.js
@@ -22,7 +22,6 @@ const cookieParser = require("cookie-parser");
 app.use(
   cors({
     origin: "https://vinaytriptrove.netlify.app",
-    // Update to match your React app's URL
     // origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"], // Allowed methods
     // allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
