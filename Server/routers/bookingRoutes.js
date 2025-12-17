d6bbed86291be543ce90529edb86663aaf8ceff3
vinay1723 <vinaykumar.17july@gmail.com>
2025-12-17 14:54:29 +0530

    initial commit

--- Server/routers/bookingRoutes.js
+++ Server/routers/bookingRoutes.js
@@ -15,9 +15,11 @@ router
   .get(bookingController.getAllBookings)
   .post(bookingController.createBooking);
 
-router
-  .route("/:id")
-  .get(bookingController.getBooking)
-  .patch(bookingController.updateBooking)
-  .delete(bookingController.deleteBooking);
+router.route("/:id").get(bookingController.allBookings);
+
+// router
+//   .route("/:id")
+//   .get(bookingController.getBooking)
+//   .patch(bookingController.updateBooking)
+//   .delete(bookingController.deleteBooking);
 module.exports = router;
