acdd6eca1d4ca406d0b9110a76159083c7536777
vinay1723 <vinaykumar.17july@gmail.com>
2026-02-13 22:22:46 +0530

    changed navigation bar

--- Server/app.js
+++ Server/app.js
@@ -21,8 +21,8 @@ const cookieParser = require("cookie-parser");
 
 app.use(
   cors({
-    // origin: "https://vinaytriptrove.netlify.app",
-    origin: "http://localhost:5173",
+    origin: "https://vinaytriptrove.netlify.app",
+    // origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"], // Allowed methods
     // allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
     credentials: true, // Allow credentials (cookies, authorization headers)
